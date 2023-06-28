d270674f98ae32c1e39d3ab5cb9a39d46f42a68b
Giorgio Fontana <giorgiofontanaj@gmail.com>
2023-06-28 17:23:42 +0200

    end project

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
+
 <div class="container my-3">
 
-    @foreach ($movies as $movie)
-        <h2>{{$movie}}</h2>
-    @endforeach
+    <div class="flex">
+        @foreach ($movies as $movie)
+
+            <div class="miaCard">
+                <h3>   {{$movie->title}}</h3>
+                <h5>   {{$movie->original_title}}</h5>
+                <p>    {{$movie->nationality}}</p>
+                <small>{{$movie->date}} </small> <br>
+                <small>vote: {{$movie->vote}}</small>
+            </div>
+                
+        @endforeach
+    </div>
 </div>
 @endsection
